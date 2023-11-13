54cfd911c764bd531300b6325aa7eea3eb2206d6
Diego Leonardo <diegoleonardo9966@gmail.com>
2023-11-13 14:50:12 -0300

    correção utf-8

--- client.py
+++ client.py
@@ -1,8 +1,9 @@
+# -*- coding: utf-8 -*-
 import socket
 
 def main():
     # Configura o cliente
-    host = '127.0.0.1'  # IP do servidor
+    host = '192.168.137.62'  # IP do servidor
     port = 5555
 
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,14 +11,14 @@ def main():
 
     while True:
         # Solicita ao usuário para inserir uma mensagem
-        message = input("Digite uma mensagem: ")
+        message = "Diego Leonardo"
 
         # Envia a mensagem para o servidor
         client.send(message.encode('utf-8'))
 
         # Recebe a resposta do servidor
         response = client.recv(1024)
-        print(f"Resposta do servidor: {response.decode('utf-8')}")
+        print("Resposta do servidor: {}".format(response.decode('utf-8')))
 
 if __name__ == "__main__":
     main()
